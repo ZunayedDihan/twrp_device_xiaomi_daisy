77a8f7419f879872aa613a4cb2b76c8409ea5c96
Hotorou <zunayeddihan@gmail.com>
2021-06-01 19:31:44 +0000

    daisy: remove string_iter variable
    * error: unused variable 'string_iter' [-Werror,-Wunused-variable]
    * also update bootctrl
    * remove calling support for build/target/product/embedded.mk as not found in source

--- omni_daisy.mk
+++ omni_daisy.mk
@@ -17,8 +17,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-$(call inherit-product, build/target/product/embedded.mk)
-
 # Inherit from device configuration
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
